c61a3724c14b9388605f8f543244d22948e117a7
cconnerolson <43979104+cconnerolson@users.noreply.github.com>
2019-03-25 01:34:59 -0500

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -1,6 +1,7 @@
 """ An automobile tire bursts sending a shock wave (assume normal shock) propagating into the ambient air which has a
 pressure P_1 and speed of sound c_1.  If the pressure behind the shock is P2 (roughly inflated tire pressure), show that
-the speed of shock propagation u_s equals the given function.
+the speed of shock propagation u_s equals the given function.  Calculate this speed given ambient air temperature 
+T_a = 303 K and tire pressure is 2 atmospheres gauge pressure P_g = 30 psi.
 """
 
 # Requirements
@@ -10,6 +11,9 @@ from math import sqrt
 
 # Given
 
+T_a = 303 # K
+P_g = 30 # psi
+
 
 def shock_propagation(c_1, gamma, P_1, P_2):  # speed of shock propagation, u_s
-    return c_1 * sqrt((gamma - 1) / (2 * gamma) + (P_2 * (gamma + 1)) / (P_1 * 2 * gamma))
+	return c_1 * sqrt((gamma - 1) / (2 * gamma) + (P_2 * (gamma + 1)) / (P_1 * 2 * gamma))
