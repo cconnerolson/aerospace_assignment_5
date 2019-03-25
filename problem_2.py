31ef76c503c5d2a39a25f5bc0c2df36b17ea334c
cconnerolson <43979104+cconnerolson@users.noreply.github.com>
2019-03-25 01:36:45 -0500

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -4,15 +4,15 @@ designed to discharge at exit Mach number of 3.5 with exit area A_e = 100 mm². 
 R = 2077 J/(kg·K).
 
 Determine:
-    -the pressure at the nozzle exit, P_e
-    -the mass flux through the device, mdot
+	-the pressure at the nozzle exit, P_e
+	-the mass flux through the device, mdot
 """
 
 # Givens
 
-P_t = 8  # MPa
-T_t = 1000  # K
+P_t = 8 # MPa
+T_t = 1000 # K
 Ma = 3.5
-A_e = 100  # mm²
-gamma = 1.66  # 𝜸
-R = 2077  # J/(kg·K)
+A_e = 100 # mm²
+gamma = 1.66 # 𝜸
+R = 2077 # J/(kg·K)
