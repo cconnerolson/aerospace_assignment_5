17d09265aa7bdf6ac5673ae75980251778845cff
cconnerolson <43979104+cconnerolson@users.noreply.github.com>
2019-03-25 01:37:47 -0500

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -3,15 +3,15 @@ front of the tip of the probe.  The measured stagnation temperature pressure is 
 altitude of z = 15 km with ambient temperature T_a = 217 K and ambient pressure P_a = 12 kPa.
 
 Find:
-    -the Mach number, Ma
-    -the velocity of the aircraft, v
-    -the static pressure behind the shock wave, P_s
-    -the stagnation pressure behind the shock wave, P_t
+	-the Mach number, Ma
+	-the velocity of the aircraft, v
+	-the static pressure behind the shock wave, P_s
+	-the stagnation pressure behind the shock wave, P_t
 """
 
 # Givens
 
-T_t = 488  # K
-z = 15  # km
-T_a = 217  # K
-P_a = 12  # kPa
+T_t = 488 # K
+z = 15 # km
+T_a = 217 # K
+P_a = 12 # kPa
