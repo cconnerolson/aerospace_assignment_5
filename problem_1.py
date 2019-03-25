9df2a29f11c7cf096f3969a4c48106ac521d440e
cconnerolson <43979104+cconnerolson@users.noreply.github.com>
2019-03-25 00:30:26 -0500

    Set up problem with problem statement. Initialized variables for given constants.

---
+++ problem_1.py
@@ -0,0 +1,17 @@
+"""A stagnation temperature probe is mounted on the nose cone of a supersonic aircraft.  A normal shock wave stands in
+front of the tip of the probe.  The measured stagnation temperature pressure is T_t = 488 K as the jet cruises at an
+altitude of z = 15 km with ambient temperature T_a = 217 K and ambient pressure P_a = 12 kPa.
+
+Find:
+    -the Mach number, Ma
+    -the velocity of the aircraft, v
+    -the static pressure behind the shock wave, P_s
+    -the stagnation pressure behind the shock wave, P_t
+"""
+
+# Givens
+
+T_t = 488  # K
+z = 15  # km
+T_a = 217  # K
+P_a = 12  # kPa
